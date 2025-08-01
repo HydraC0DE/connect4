2fb622003afba016d0b32d8a25f1026638ebe532
HydraC0DE <szaboakos0409@gmail.com>
2025-08-01 21:29:54 +0200

    added empty ai py file and dummy page, game logic cleanup

--- app/routes/game.py
+++ app/routes/game.py
@@ -1,7 +1,21 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, jsonify
 
 game = Blueprint("game", __name__)
 
 @game.route("/game")
 def game_page():
     return render_template("game.html")
+
+@game.route("/game/state")
+def game_state():
+    # dummy board
+    dummy_board = [
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 1, 2, 0, 0, 0],
+        [0, 2, 1, 1, 2, 0, 0],
+        [0, 1, 2, 1, 1, 2, 0],
+        [0, 0, 0, 2, 1, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+    ]
+    current_player = "YELLOW"
+    return jsonify(board=dummy_board, current_player=current_player)
