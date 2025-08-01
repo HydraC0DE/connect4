79002b7b411ea2fc01e463780183225d13a05ce7
HydraC0DE <szaboakos0409@gmail.com>
2025-08-01 18:37:35 +0200

    game terminal checks

--- app/games/connect4.py
+++ app/games/connect4.py
@@ -12,8 +12,12 @@ class Connect4:
         self.board = [[0 for _ in range(self.cols)] for _ in range(self.rows)]
         self.current_player = Player.YELLOW ##yellow goes first according to connect4 rules
         self.move_count = 0
+        self.game_over = False 
 
     def make_move(self, column):
+        if getattr(self, "game_over", False):
+            raise ValueError("Game is already over.")
+        
         # Drop piece into column
         # Return (row, col) of move or raise error if full
         if column < 7 and column>-1:
@@ -23,17 +27,34 @@ class Connect4:
                 if self.board[row][column] == 0:
                     self.board[row][column] = self.current_player.value
                     self.move_count += 1
-                    if self.is_terminal(row, column, self.current_player.value) or self.check_draw():
-                        self.finalizeMatch() ##give back winner information, maybe other stuff
+                    if self.is_terminal_win(row, column, self.current_player.value):
+                        self.finalizeMatchWin(self.current_player) ##give back winner information, maybe other stuff
+                    elif self.is_terminal_draw():
+                        self.finalizeMatchDraw()
+                    else:
+                        self.switch_player()
                     return row, column
 
-    def is_terminal(self, current_row_pos, current_col_pos, colorValue):
-        x = 0
-        y = 0
-        sameColorCount = 0
-        while 0 <= current_row_pos + x < 6 and 0 <= current_col_pos + y < 7:
-            if
+    def is_terminal_win(self, current_row_pos, current_col_pos, colorValue):
+        return (
+        self.check_horizontal(current_row_pos, current_col_pos, colorValue)
+        or self.check_vertical(current_row_pos, current_col_pos, colorValue)
+        or self.check_diagonal1(current_row_pos, current_col_pos, colorValue)
+        or self.check_diagonal2(current_row_pos, current_col_pos, colorValue)
+        # dont check for draw we do that somewhere different
+        )
+    
+    def is_terminal_draw(self):
+        return self.move_count == self.rows * self.cols
             
+    def finalizeMatchWin(self, winner_color):
+        self.winner = winner_color
+        self.game_over = True
+
+    def finalizeMatchDraw(self):
+        self.winner = None
+        self.game_over = True
+
     def check_horizontal(self, current_row_pos, current_col_pos, colorValue):
         #check left then right and sum
         x=-1
@@ -118,8 +139,7 @@ class Connect4:
         return sameColorCount >= 4
 
 
-    def check_draw(self):
-        return self.move_count == self.rows * self.cols
+    
 
     def switch_player(self):
         self.current_player = Player.RED if self.current_player == Player.YELLOW else Player.YELLOW
